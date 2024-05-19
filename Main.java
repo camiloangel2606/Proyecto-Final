94e535455b7501443c5f6b64d06648dbba87fd76
camiloangel2606 <163881091+camiloangel2606@users.noreply.github.com>
2024-05-18 20:17:47 -0500

    Update Patient Final and Main

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Patient.addPatient(1, "John Doe", 30, 70.5f, 180.2f, new String[] { "Diabetes", "Hypertension" });
         Patient.addPatient(2, "Jane Smith", 25, 60.0f, 170.5f, new String[] { "Asthma" });
-
         Patient.printPatientList();
     }
 }
