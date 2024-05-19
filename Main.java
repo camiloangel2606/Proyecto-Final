7636a9aa787af1b612d2c5ba01d6df24e97941d2
camiloangel2606 <163881091+camiloangel2606@users.noreply.github.com>
2024-05-18 21:30:07 -0500

    Update Patient final

--- Main.java
+++ Main.java
@@ -1,7 +1,93 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Patient.addPatient(1, "John Doe", 30, 70.5f, 180.2f, new String[] { "Diabetes", "Hypertension" });
         Patient.addPatient(2, "Jane Smith", 25, 60.0f, 170.5f, new String[] { "Asthma" });
         Patient.printPatientList();
+
+        // Menú:
+        System.out.println("Bienvenido al sistema de Dietas para Pacientes.");
+        Scanner scanner = new Scanner(System.in);
+        int i;
+        do {
+            System.out.println("Selecciona la función a realizar:");
+            System.out.println("(1) Gestión de Pacientes.");
+            System.out.println("(2) Gestión de Nutricionistas.");
+            System.out.println("(3) Plan de Alimentación.");
+            System.out.println("(4) Registros de Comidas.");
+            System.out.println("(0) Salir.");
+            i = Integer.parseInt(scanner.nextLine()); // Leemos el valor para saber la función.
+
+            if (i == 1) { // Agregar, actualizar y eliminar información de pacientes.
+                int j;
+                do {
+                    System.out.println("Ingresa la función a realizar:");
+                    System.out.println("(1) Registrar un paciente nuevo.");
+                    System.out.println("(2) Actualizar información de un paciente.");
+                    System.out.println("(3) Eliminar la información de un paciente.");
+                    System.out.println("(0) Regresar al menú principal.");
+                    j = Integer.parseInt(scanner.nextLine()); // Leemos el valor para saber la función.
+
+                    if (j == 1) {
+                        System.out.println("Escribe el id del paciente.");
+                        int patientId = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Escribe el nombre completo del paciente.");
+                        String fullName = scanner.nextLine();
+                        System.out.println("Escribe la edad del paciente.");
+                        int age = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Escribe el peso del paciente.");
+                        float weight = Float.parseFloat(scanner.nextLine());
+                        System.out.println("Escribe la altura del paciente.");
+                        float height = Float.parseFloat(scanner.nextLine());
+                        System.out.println("Escribe las condiciones preexistentes separándolas con ','");
+                        String preexistingConditions = scanner.nextLine();
+
+                        // Divide la entrada en partes individuales usando el delimitador ","
+                        String[] preexistingConditionsArray = preexistingConditions.split(",");
+
+                        // Elimina los espacios en blanco alrededor de cada condición
+                        for (int k = 0; k < preexistingConditionsArray.length; k++) {
+                            preexistingConditionsArray[k] = preexistingConditionsArray[k].trim();
+                        }
+
+                        Patient.addPatient(patientId, fullName, age, weight, height, preexistingConditionsArray);
+
+                    } else if (j == 2) {
+                        System.out.println("Escribe el id del paciente a actualizar.");
+                        int patientId = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Escribe el nuevo nombre completo del paciente.");
+                        String fullName = scanner.nextLine();
+                        System.out.println("Escribe el año de nacimiento del paciente.");
+                        int age = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Escribe el nuevo peso del paciente.");
+                        float weight = Float.parseFloat(scanner.nextLine());
+                        System.out.println("Escribe la nueva altura del paciente.");
+                        float height = Float.parseFloat(scanner.nextLine());
+                        System.out.println("Escribe las nuevas condiciones preexistentes separándolas con ','");
+                        String preexistingConditions = scanner.nextLine();
+
+                        // Divide la entrada en partes individuales usando el delimitador ","
+                        String[] preexistingConditionsArray = preexistingConditions.split(",");
+
+                        // Elimina los espacios en blanco alrededor de cada condición
+                        for (int k = 0; k < preexistingConditionsArray.length; k++) {
+                            preexistingConditionsArray[k] = preexistingConditionsArray[k].trim();
+                        }
+
+                        Patient.updatePatient(patientId, fullName, age, weight, height, preexistingConditionsArray);
+
+                    } else if (j == 3) {
+                        System.out.println("Escribe el id del paciente a eliminar.");
+                        int patientId = Integer.parseInt(scanner.nextLine());
+                        Patient.removePatient(patientId);
+                    }
+                } while (j != 0);
+            } else if (i == 2) { // Registro y gestión de nutricionistas.
+                // Lógica para gestión de nutricionistas.
+            }
+        } while (i != 0);
+        Patient.printPatientList();
+        scanner.close();
     }
 }
--- Patient.java
+++ Patient.java
@@ -1,69 +1,67 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Patient {
-    // Campos para almacenar la información del paciente
-    private int PatientId;
-    private String FullName;
-    private int Age;
-    private float Weight;
-    private float Height;
+    private int patientId;
+    private String fullName;
+    private int age;
+    private float weight;
+    private float height;
     private List<String> preexistingConditions;
 
-    // Lista estática para almacenar todos los pacientes
     private static List<Patient> patientList = new ArrayList<>();
 
-    // Constructor para inicializar los campos del paciente
-    public Patient(int PatientId, String FullName, int Age, float Weight, float Height,
+    public Patient(int patientId, String fullName, int age, float weight, float height,
             List<String> preexistingConditions) {
-        this.PatientId = PatientId;
-        this.FullName = FullName;
-        this.Age = Age;
-        this.Weight = Weight;
-        this.Height = Height;
+        this.patientId = patientId;
+        this.fullName = fullName;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
         this.preexistingConditions = preexistingConditions;
     }
 
     // Métodos getters y setters para los campos
     public int getPatientId() {
-        return PatientId;
+        return patientId;
     }
 
-    public void setPatientId(int PatientId) {
-        this.PatientId = PatientId;
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
     }
 
     public String getFullName() {
-        return FullName;
+        return fullName;
     }
 
-    public void setFullName(String FullName) {
-        this.FullName = FullName;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public int getAge() {
-        return Age;
+        return age;
     }
 
-    public void setAge(int Age) {
-        this.Age = Age;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public float getWeight() {
-        return Weight;
+        return weight;
     }
 
-    public void setWeight(float Weight) {
-        this.Weight = Weight;
+    public void setWeight(float weight) {
+        this.weight = weight;
     }
 
     public float getHeight() {
-        return Height;
+        return height;
     }
 
-    public void setHeight(float Height) {
-        this.Height = Height;
+    public void setHeight(float height) {
+        this.height = height;
     }
 
     public List<String> getPreexistingConditions() {
@@ -74,27 +72,56 @@ public class Patient {
         this.preexistingConditions = preexistingConditions;
     }
 
-    // Método para añadir un paciente a la lista
-    public static void addPatient(int PatientId, String FullName, int Age, float Weight, float Height,
-            String[] PreexistingConditions) {
-        List<String> preexistingConditionsList = Arrays.asList(PreexistingConditions);
-        Patient newPatient = new Patient(PatientId, FullName, Age, Weight, Height, preexistingConditionsList);
-        patientList.add(newPatient);
+    @Override
+    public String toString() {
+        return "Patient[PatientId:" + patientId + ", FullName:" + fullName + ", Age:" + age + ", Weight:" + weight
+                + ", Height:" + height + ", PreexistingConditions:" + preexistingConditions + "]";
     }
 
-    // Método para mostrar la lista de pacientes
+    public static void addPatient(int patientId, String fullName, int age, float weight, float height,
+            String[] preexistingConditions) {
+        List<String> preexistingConditionsList = Arrays.asList(preexistingConditions);
+        Patient newPatient = new Patient(patientId, fullName, age, weight, height, preexistingConditionsList);
+        for (Iterator<Patient> iterator = patientList.iterator(); iterator.hasNext();) {
+            Patient patient = iterator.next();
+            if (patient.getPatientId() == patientId) {
+                System.out.println("El paciente ya se encuentra agregado.");
+            }
+        }
+        patientList.add(newPatient);
+        System.out.println("Se agregó correctamentamente al paciente.");
+    }
+
+    public static void removePatient(int PatientId) {
+        for (Iterator<Patient> iterator = patientList.iterator(); iterator.hasNext();) {
+            Patient patient = iterator.next();
+            if (patient.getPatientId() == PatientId) {
+                iterator.remove(); // Elimina el paciente de la lista
+                System.out.println("Paciente eliminado exitosamente.");
+                return; // Salir del método una vez que se elimine el paciente
+            }
+        }
+        System.out.println("No se encontró ningún paciente con el ID especificado.");
+    }
+
+    public static void updatePatient(int patientId, String fullName, int age, float weight, float height,
+            String[] preexistingConditions) {
+        for (Patient patient : patientList) {
+            if (patient.getPatientId() == patientId) {
+                patient.setFullName(fullName);
+                patient.setAge(age);
+                patient.setWeight(weight);
+                patient.setHeight(height);
+                patient.setPreexistingConditions(Arrays.asList(preexistingConditions));
+                break;
+            }
+        }
+        System.out.println("Se actualizó correctamente la información del paciente.");
+    }
+
     public static void printPatientList() {
         for (Patient patient : patientList) {
             System.out.println(patient);
         }
-    }
-
-    // Método toString para una representación en cadena de la información del
-    // paciente
-    @Override
-    public String toString() {
-        return "Patient[PatientId:" + PatientId + ", FullName:" + FullName + ", Age:" + Age + ", Weight:" + Weight
-                + ", Height:" + Height
-                + ", PreexistingConditions:" + preexistingConditions + "]";
     }
 }
